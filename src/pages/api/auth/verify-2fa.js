5e125e16ecb265f3a7f82e770273a6bf298d9c90
mddanish867 <mddanish867@gmail.com>
2024-09-03 01:02:44 +0530

    modified 2fa to verify user

--- src/pages/api/auth/verify-2fa.js
+++ src/pages/api/auth/verify-2fa.js
@@ -1,21 +1,21 @@
-import { PrismaClient } from "@prisma/client";
 import speakeasy from "speakeasy";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowd" });
+    return res.status(405).json({ message: "Method not allowed" });
   }
 
-  const { userId, token } = req.body;
+  const { userId } = req.body;
 
-  if (!userId || !token) {
-    return res.status(400).json({ message: "User ID and token are required" });
+  if (!userId) {
+    return res.status(400).json({ message: "User ID  is required." });
   }
 
   try {
-    // Retrive the User and their 2FA secret
+    // Retrieve the User and their 2FA secret
     const user = await prisma.user.findUnique({ where: { id: userId } });
 
     if (!user || !user.twoFactorSecret) {
@@ -24,19 +24,24 @@ export default async function handler(req, res) {
         .json({ message: "2FA is not enabled for this user" });
     }
 
-    // Verify the token
+    const generatedToken = speakeasy.totp({
+      secret: user.twoFactorSecret,
+      encoding: "base32",
+    });
+
     const isVerified = speakeasy.totp.verify({
       secret: user.twoFactorSecret,
       encoding: "base32",
-      token,
+      token: generatedToken,
     });
 
     if (!isVerified) {
-      res.status(400).json({ message: "Invalid token" });
+      return res.status(400).json({ message: "Invalid token" });
     }
-    res.status(200).json({ message: "2FA verified successfully" });
+
+    return res.status(200).json({ message: "2FA verified successfully" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error("Verification error:", error); // Log the error for debugging
+    return res.status(500).json({ message: "Internal server error" });
   }
 }
