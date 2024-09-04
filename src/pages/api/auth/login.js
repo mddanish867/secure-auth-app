244d15303992e96187ad7d491ce6a97d6b23bc2d
mddanish867 <mddanish867@gmail.com>
2024-09-04 22:25:37 +0530

    modified the login api

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   } 
-  
+
   if (req.method !== "POST") {
     return res.status(405).json({
       message: "Method not allowed",
@@ -72,6 +72,7 @@ export default async function handler(req, res) {
   // Return the response
   return res.status(200).json({
     message: 'Logged in successfully',
+    token: token,
     success: true,    
   });
 
