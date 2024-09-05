565464990a414d8d3e2fc3500e0425272586ea94
mddanish867 <mddanish867@gmail.com>
2024-09-05 13:37:15 +0530

    added cors in login api

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust this to allow specific origins
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   // Handle preflight OPTIONS request
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
