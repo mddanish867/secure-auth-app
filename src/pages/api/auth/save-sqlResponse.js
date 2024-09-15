71bda537f212ebdbe9a78658f4c67a9438ebf36a
mddanish867 <mddanish867@gmail.com>
2024-09-16 01:25:54 +0530

    modifed the save response api

--- src/pages/api/auth/save-sqlResponse.js
+++ src/pages/api/auth/save-sqlResponse.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
     });
   }
 
-  const { query, response } = req.body;
+  const {userId, query, response } = req.body;
 
   if (!query || !response) {
     return res.status(400).json({ message: "Query and response are required." });
