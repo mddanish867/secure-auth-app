948930c9553b2e6214f63c3764a0054877b00920
mddanish867 <mddanish867@gmail.com>
2024-10-19 12:21:26 +0530

    added cors policy to create templates route

--- src/pages/api/templates/create.js
+++ src/pages/api/templates/create.js
@@ -10,16 +10,26 @@ export const config = {
   },
 };
 
-export default async function handler(req, res) {
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Adjust this to allow specific origins
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://anjumara-saas-application.vercel.app',
+  ];
 
-  // Handle preflight OPTIONS request
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
+export default async function handler(req, res) {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', ''); // Or handle unauthorized origins
   }
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  } 
 
   if (req.method !== "POST") {
     return res.status(405).json({
